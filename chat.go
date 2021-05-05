26cc4c13747721fe53abe5befdd1f38d8ed5a193
sonien.son <sonien.son@kakaoenterprise.com>
2021-05-06 07:20:20 +0900

    WIP

--- chat.go
+++ chat.go
@@ -1,20 +1,229 @@
 package main
 
 import (
+	"container/list"
+	"context"
+	"fmt"
 	"github.com/googollee/go-socket.io"
 	"log"
 	"net/http"
+	"sync"
+	"time"
 )
 
+// Configuration
+const (
+	SOCKCLIENT_EMIT_TIMEOUT = time.Second
+	SOCKCLIENT_EMIT_BUFFER  = 10
+
+	CHATROOM_EVENT_BUFFER = 10
+	CHATROOM_CONN_BUFFER  = 1
+	CHATROOM_DISC_BUFFER  = 1
+)
+
+// Notice
+const (
+	NOTICE_SHUTDOWN = "System will be shutdown soon."
+)
+
+// EventType
+const (
+	SUBSCRIBE = iota
+	UNSUBSCRIBE
+	MESSAGE
+)
+
+var EventType = [...]string{
+	"SUBSCRIBE",
+	"UNSUBSCRIBE",
+	"MESSAGE",
+}
+
+type Event struct {
+	EventType string `json:"event_type"`
+	Timestamp int    `json:"timestamp"` // Server processed time for outbound, otherwise 0.
+	Id        string `json:"id"`        // Empty for inbound or notice outbound, otherwise user id.
+	Message   string `json:"message"`   // Content for EventType[MESSAGE].
+}
+
+func newEvent(eventType string, id string, message string) *Event {
+	event := new(Event)
+	event.EventType = eventType
+	event.Timestamp = int(time.Now().Unix())
+	event.Id = id
+	event.Message = message
+
+	return event
+}
+
+func newNoticeEvent(message string) *Event {
+	return newEvent(EventType[MESSAGE], "", "message")
+}
+
+// socket.io client
+type SockClient struct {
+	s      socketio.Conn
+	events chan Event
+	done   chan struct{}
+}
+
+// subscribe create new SockClient instance.
+func subscribe(s socketio.Conn) *SockClient {
+	socketClient := new(SockClient)
+	socketClient.s = s
+	socketClient.done = make(chan struct{})
+	socketClient.events = make(chan Event, SOCKCLIENT_EMIT_BUFFER)
+
+	socketClient.run()
+
+	return socketClient
+}
+
+// run launches looper of SockClient. Accents Event and emit to browser client.
+// Looper terminates when channel done has been closed.
+func (sc *SockClient) run() {
+	go func(sc *SockClient) {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Println(fmt.Sprintf("SockClient %3d : Unexpected termination"))
+			}
+		}()
+
+		log.Println(fmt.Sprintf("SockClient %3s : MAIN LOOP start", sc.s.ID()))
+	MainLoop:
+		for {
+			select {
+			case <-sc.done:
+				break MainLoop
+			case event := <-sc.events:
+				sc.s.Emit("event", event)
+			}
+		}
+		log.Println(fmt.Sprintf("SockClient %3s : MAIN LOOP terminate", sc.s.ID()))
+	}(sc)
+}
+
+// post asynchronous puts Event to channel.
+func (sc *SockClient) post(pEvent *Event) {
+	go func(sc *SockClient, event Event) {
+		select {
+		case sc.events <- event:
+		case <-time.After(SOCKCLIENT_EMIT_TIMEOUT):
+			log.Println(fmt.Sprintf("SockClient %3s : Timeout", sc.s.ID()))
+		}
+	}(sc, *pEvent)
+}
+
+// unsubscribe request termination of looper and close all channels initiated in subscribe().
+func (sc *SockClient) unsubscribe() {
+	close(sc.done)
+	close(sc.events)
+}
+
+//
 func main() {
-	server := socketio.NewServer(nil)
-	server.OnConnect("/", func(s socketio.Conn) error {
-		s.SetContext("")
+	connection := make(chan socketio.Conn, CHATROOM_CONN_BUFFER)
+	disconnection := make(chan socketio.Conn, CHATROOM_DISC_BUFFER)
+	events := make(chan Event, CHATROOM_EVENT_BUFFER)
+	done := make(chan struct{})
+
+	// socket.io 서버
+	sockServer := socketio.NewServer(nil)
+	sockServer.OnConnect("/", func(s socketio.Conn) error {
 		log.Println("connected:", s.ID())
+		connection <- s
 		return nil
 	})
 
-	http.Handle("/socket.io/", server)
+	sockServer.OnDisconnect("/", func(s socketio.Conn, reason string) {
+		log.Println(s.ID(), "is disconnected by", reason)
+		disconnection <- s
+	})
+
+	sockServer.OnEvent("/", "event", func(s socketio.Conn, event Event) string {
+		event.Id = s.ID()
+		event.Timestamp = int(time.Now().Unix())
+		events <- event
+
+		return "200"
+	})
+
+	// Launch : socket.io Server.
+	go sockServer.Serve()
+
+	// Launch : host looper.
+	go func() {
+		sockClients := list.New()
+	MainLoop:
+		for {
+			select {
+			case s := <-connection:
+				pEvent := newEvent(EventType[SUBSCRIBE], s.ID(), "")
+				for iter := sockClients.Front(); iter != nil; iter = iter.Next() {
+					sockClient := iter.Value.(*SockClient)
+					sockClient.post(pEvent)
+				}
+				sockClient := subscribe(s)
+				sockClients.PushBack(sockClient)
+			case s := <-disconnection:
+				pEvent := newEvent(EventType[UNSUBSCRIBE], s.ID(), "")
+				for iter := sockClients.Front(); iter != nil; iter = iter.Next() {
+					sockClient := iter.Value.(*SockClient)
+					if sockClient.s == s {
+						sockClients.Remove(iter)
+						sockClient.unsubscribe()
+					} else {
+						sockClient.post(pEvent)
+					}
+				}
+			case event := <-events:
+				for iter := sockClients.Front(); iter != nil; iter = iter.Next() {
+					sockClient := iter.Value.(*SockClient)
+					if sockClient.s.ID() == event.Id {
+						continue
+					} else {
+						sockClient.post(&event)
+					}
+				}
+			case <-done:
+				break MainLoop
+			}
+		}
+
+		// Server is shutting down. Terminate all sockClients
+		pEvent := newNoticeEvent(NOTICE_SHUTDOWN)
+
+		wg := sync.WaitGroup{}
+		for iter := sockClients.Front(); iter != nil; iter = iter.Next() {
+			wg.Add(1)
+
+			sockClient := iter.Value.(*SockClient)
+			sockClient.post(pEvent)
+
+			go func(sockClient *SockClient) {
+				sockClient.unsubscribe()
+				wg.Done()
+			}(sockClient)
+		}
+		wg.Wait()
+
+	}()
+
+	// 웹서버
+	var httpServer = &http.Server{
+		Addr:    ":8000",
+		Handler: nil,
+	}
+	http.Handle("/socket.io/", sockServer)
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	http.ListenAndServe(":80", nil)
+	http.HandleFunc("/terminate", func(writer http.ResponseWriter, _ *http.Request) {
+		log.Println("Server shutting down.")
+		done <- struct{}{}
+		sockServer.Close()
+		httpServer.Shutdown(context.Background())
+	})
+
+	log.Println("Serving at localhost:8000.")
+	log.Fatal(httpServer.ListenAndServe())
+	log.Println("Server terminated.")
 }
